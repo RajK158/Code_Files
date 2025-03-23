56ac923972c5f1f6a8c7d0b66a36b5bdb0e03a38
Raj Kundur <raj.kundur01@student.csulb.edu>
2025-03-23 13:30:41 -0700

    Simple scanner class program

---
+++ scanner_class.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class scanner_class {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter you ID number: ");
+        int num = scanner.nextInt();
+        System.out.println("Name: " +name+ " ID: " +num);
+        System.out.println("Data added succesfully");
+        
+    }
+}
+// The scanner object: Scanner scanner = new Scanner(System.in); sets up the scanner to read input from the user.
+
+// In our program example, we used the scanner.nextLine() method to read strings, but we can also use
+
+// scanner.nextDouble()
+// scanner.nextBoolean()
+// scanner.nextInt()
