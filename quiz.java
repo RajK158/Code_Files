dc0215f45e97a8f08b3a4972f73b4db4983d1c9d
Raj Kundur <raj.kundur01@student.csulb.edu>
2025-03-23 18:30:26 -0700

    Solve the Riddle

---
+++ quiz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class quiz{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?");
+       
+        String quiz = scanner.nextLine();
+        System.out.println("Your answer: " +quiz);
+        if (quiz.equalsIgnoreCase("Echo"))
+        {
+            System.out.println("Congratulations your answer is correct");
+        }
+        else {
+            System.out.println("Incorrect... try again");
+        }
+    }    
+    }
